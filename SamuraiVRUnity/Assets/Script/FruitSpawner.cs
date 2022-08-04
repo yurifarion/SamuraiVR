2a7e10fbd1c5d24687b02262f56e2cfe4a9ff9d0
Yuri Farion <yuriruf@hotmail.com>
2022-08-04 19:23:08 +0900

    Samurai Ice Slicer
    Had a problem with the Slicer script with other geometric than cubes, so now i changed the game to Slicing ice cubes not fruits.
    Add slicer script and a simple spawner

---
+++ SamuraiVRUnity/Assets/Script/FruitSpawner.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitSpawner : MonoBehaviour
+{
+    // this class wil spawn fruits randomly within a constant time interval
+    public GameObject fruitPrefab;
+    public float timeInterval = 1;
+    private float timer = 0;
+
+    //Spawner Points
+    public Transform spawnerPoint1;
+    public Transform spawnerPoint2;
+    public Transform spawnerPoint3;
+    public Transform spawnerPoint4;
+
+    public float pushStrength = 15;
+    public bool readyToStart = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = timeInterval;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (readyToStart && timer <= 0)
+        {
+          
+
+            //pick randomly one spawnerPoint
+            int rand = Random.Range(1, 5);
+            Transform spawnTransform = spawnerPoint1;
+            if (rand == 2) spawnTransform = spawnerPoint2;
+            if (rand == 3) spawnTransform = spawnerPoint3;
+            if (rand == 4) spawnTransform = spawnerPoint4;
+
+            GameObject spawn = Instantiate(fruitPrefab, spawnTransform.position, spawnTransform.rotation);
+            //this prefab should have a rigidBody
+            spawn.GetComponent<Rigidbody>().AddForce(new Vector3(pushStrength, 0, 0));  //make it go forward to player
+
+            Vector3 randonRotation = new Vector3(Random.Range(0, 5), Random.Range(0, 5), Random.Range(0, 5));
+            spawn.GetComponent<Rigidbody>().AddTorque(randonRotation * 100);  //make it rotate
+            timer = timeInterval;
+        }
+        else timer -= Time.deltaTime;
+    }
+}
---
+++ SamuraiVRUnity/Assets/Script/KatanaSlicer.cs
@@ -0,0 +1,80 @@
+using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KatanaSlicer : MonoBehaviour
+{
+    //Tip and Base will feed the Slice class to create two new GameObjects from the slice. the positive up and negative down
+    public GameObject _tip;
+    public GameObject _base;
+    private Vector3 _triggerEnterTipPosition;
+    private Vector3 _triggerEnterBasePosition;
+    private Vector3 _triggerExitTipPosition;
+
+    //after the cut this is the force that will be applyied to the object
+    private float _forceAppliedToCut = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        _triggerEnterTipPosition = _tip.transform.position;
+        _triggerEnterBasePosition = _base.transform.position;
+    }
+
+    // in order to know if the blade is or not cutting something is determined by trigger;
+    private void OnTriggerExit(Collider other)
+    {
+        _triggerExitTipPosition = _tip.transform.position;
+
+        //Create a triangle between the tip and base so that we can get the normal
+        Vector3 side1 = _triggerExitTipPosition - _triggerEnterTipPosition;
+        Vector3 side2 = _triggerExitTipPosition - _triggerEnterBasePosition;
+
+        //Get the point perpendicular to the triangle above which is the normal
+        //https://docs.unity3d.com/Manual/ComputingNormalPerpendicularVector.html
+        Vector3 normal = Vector3.Cross(side1, side2).normalized;
+
+        //Transform the normal so that it is aligned with the object we are slicing's transform.
+        Vector3 transformedNormal = ((Vector3)(other.gameObject.transform.localToWorldMatrix.transpose * normal)).normalized;
+
+        //Get the enter position relative to the object we're cutting's local transform
+        Vector3 transformedStartingPoint = other.gameObject.transform.InverseTransformPoint(_triggerEnterTipPosition);
+
+        Plane plane = new Plane();
+
+        plane.SetNormalAndPosition(
+                transformedNormal,
+                transformedStartingPoint);
+
+        var direction = Vector3.Dot(Vector3.up, transformedNormal);
+
+        //Flip the plane so that we always know which side the positive mesh is on
+        if (direction < 0)
+        {
+            plane = plane.flipped;
+        }
+
+        GameObject[] slices = Slicer.Slice(plane, other.gameObject);
+
+        //Make sure that every gameObject after sliced will be destroied after 1 sec
+        foreach (GameObject o in slices)
+        {
+            Destroy(o,1);
+        }
+        Destroy(other.gameObject);
+
+        Rigidbody rigidbody = slices[1].GetComponent<Rigidbody>();
+        Vector3 newNormal = transformedNormal + Vector3.up * _forceAppliedToCut;
+        rigidbody.AddForce(newNormal, ForceMode.Impulse);
+    }
+}
